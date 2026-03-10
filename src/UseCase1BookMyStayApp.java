cabd2625307faf40056ddf93aab0bc7d8fb2cdd2
anvita104 <anvitaanil55@gmail.com>
2026-03-10 08:57:38 +0530

    UC1 - Application Entry and Welcome Message for BookMyStayApp

---
+++ src/UseCase1BookMyStayApp.java
@@ -0,0 +1,22 @@
+/**
+ * ==========================================================
+ * MAIN CLASS - UseCase1BookMyStayApp
+ * ==========================================================
+ *
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * @author Developer
+ * @version 1.0
+ */
+
+public class UseCase1BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=====================================");
+        System.out.println("Welcome to BookMyStayApp");
+        System.out.println("Hotel Booking System v1.0");
+        System.out.println("=====================================");
+        System.out.println("Application started successfully.");
+    }
+}
