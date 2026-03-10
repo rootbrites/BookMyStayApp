53bad1abc8b46045496d7857036662b85f43e9d3
rootbrites <bhaskara.skvamshitha@gmail.com>
2026-03-10 14:46:51 +0530

    Base application setup

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,16 @@
+/**
+ * Use Case 1: Application Entry & Welcome Message
+ * Book My Stay App
+ */
+
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" Welcome to Book My Stay ");
+        System.out.println(" Hotel Booking System v1.0 ");
+        System.out.println("=================================");
+
+    }
+}
